1677ca87f618bc84cb77af91c253b97b672451dc
Katrina <kat.kopio@gmail.com>
2021-11-22 21:38:16 +0800

    Set up app

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/sendtime")
+def sendtime():
+    return render_template('sendtime.html')
+
+@app.route("/view")
+def view():
+    return render_template('viewresponse.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
